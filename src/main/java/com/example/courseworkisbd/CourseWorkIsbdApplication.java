6f9b66884b6353916b5d15c76b3e068e272761fa
wizarsi <>
2022-12-13 20:20:10 +0300

    Initial commit

---
+++ src/main/java/com/example/courseworkisbd/CourseWorkIsbdApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkisbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkIsbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkIsbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseworkisbd/CourseWorkIsbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkisbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkIsbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
